c453823e9ced494b52c13967f549fe17a799e3ac
Itachiprox <157369200+Itachiprox@users.noreply.github.com>
2024-01-24 14:37:50 +0530

    Update config.py

--- shivu/config.py
+++ shivu/config.py
@@ -2,18 +2,18 @@ class Config(object):
     LOGGER = True
 
     # Get this value from my.telegram.org/apps
-    OWNER_ID = "6765826972"
-    sudo_users = "6845325416", "6765826972"
-    GROUP_ID = -1002133191051
-    TOKEN = "6707490163:AAHZzqjm3rbEZsObRiNaT7DMtw_i5WPo_0o"
-    mongo_url = "mongodb+srv://HaremDBBot:ThisIsPasswordForHaremDB@haremdb.swzjngj.mongodb.net/?retryWrites=true&w=majority"
+    OWNER_ID = "5490773419"
+    sudo_users = "5228565677", "6590287973"
+    GROUP_ID = -1002037503706
+    TOKEN = "6878148789:AAEjZJTGRDyfOv5eD1kWn6f7mu7YDgMJeyc"
+    mongo_url = "mongodb+srv://itachi:itachi@itachi.hyhnjlq.mongodb.net/?retryWrites=true&w=majority"
     PHOTO_URL = ["https://telegra.ph/file/b925c3985f0f325e62e17.jpg", "https://telegra.ph/file/4211fb191383d895dab9d.jpg"]
-    SUPPORT_CHAT = "Collect_em_support"
-    UPDATE_CHAT = "Collect_em_support"
-    BOT_USERNAME = "Collect_Em_AllBot"
-    CHARA_CHANNEL_ID = "-1002133191051"
-    api_id = 26626068
-    api_hash = "bf423698bcbe33cfd58b11c78c42caa2"
+    SUPPORT_CHAT = "waifu_support"
+    UPDATE_CHAT = "catchlogs"
+    BOT_USERNAME = "Catchyourwaifubot"
+    CHARA_CHANNEL_ID = "-1002037503706"
+    api_id = 24658337
+    api_hash = "bf99242dbb7f3501f28d39f0a0383cbf"
 
     
 class Production(Config):
